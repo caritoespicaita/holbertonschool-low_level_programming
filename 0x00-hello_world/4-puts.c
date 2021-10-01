954fd06eb2f7f8932222f355687047686ce9cfb6
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-01 03:36:35 +0000

    4-puts.c fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  *  * Description: main - Prints out a string to stdout.
- *   *   * Return: 0 if success.
- *    *    */
-
-main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-        returns(0);
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{          
+        puts("\"Programming is like building a multilingual puzzle");
+	:wqreturn (0);
 }
