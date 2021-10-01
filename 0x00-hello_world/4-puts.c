c357fbfbaa65f5255ecaee5ab53f8e635abf0c07
caritoespicaita <caritoespicaita@hotmail.com>
2021-09-30 22:38:53 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,17 +6,8 @@
  *    * Return: Always 0 (Success)
  *     */
 int main(void)
-{          
-        puts("\"Programming is like building a multilingual puzzle");
-	:wqreturn (0);
-=======
- *  *  * Description: main - Prints out a string to stdout.
- *   *   * Return: 0 if success.
- *    *    */
-
-int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
         returns(0);
->>>>>>> 4a659edd4f493eab2cc9b64b0b56539e5bfbd875
+
 }
