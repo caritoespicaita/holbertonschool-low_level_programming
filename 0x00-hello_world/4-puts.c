3250d8ec2ca042f34f90ccd42db1e0ddef4e29bd
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-01 03:30:59 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  *  * Description: main - Prints out a string to stdout.
+ *   *   * Return: 0 if success.
+ *    *    */
+
+main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        returns(0);
+}
