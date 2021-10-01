292186e788452c37aff2badaa5cdbf98004c64b3
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-01 04:03:27 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
+int main(void)
+{
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
+	return (0);
+}
