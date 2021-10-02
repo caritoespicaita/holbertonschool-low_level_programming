27b4ba7e16f4b286a40033ce57212f9614380a0e
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-01 22:14:16 -0500

    3.alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,9 +7,9 @@ int main(void)
 {
 int var;
 int var1;
-for (var= 'a'; var <= 'z'; var++)
+for (var = 'a'; var <= 'z'; var++)
 putchar(var);
-for (var1= 'A'; var1 <= 'Z'; var1++)
+for (var1 = 'A'; var1 <= 'Z'; var1++)
 putchar(var1);
 putchar('\n');
 return (0);
