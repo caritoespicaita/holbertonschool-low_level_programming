06f795214203ad53c5851dd3fc73107ff1d9036c
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-02 03:33:41 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int var;
+int var1;
+int var2;
+for (var = 'a'; var <= 'd'; var++)
+putchar(var);
+for (var1 = 'f'; var1 <= 'p'; var1++)
+putchar(var1);
+for (var2 = 'r'; var2 <= 'z'; var2++)
+putchar(var2);
+putchar('\n');
+return (0);
+}
