c8640063a214f9c0e55bbfc59274c3ee1c34c728
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-01 21:24:09 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - assign a random number to the variable n each time it is executed
-* 
-*  
+* main - assign a random number to the variable n each time it is executed 
 * Return: 0
 */
 
