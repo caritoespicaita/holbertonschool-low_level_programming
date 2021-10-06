b242f7f6a05976fe607c5278c5fcd12fdadd47e4
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-06 03:51:58 +0000

    10-add.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,5 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
 
 #endif
