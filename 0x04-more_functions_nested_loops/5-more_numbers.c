641c05fb8753d62168b05efc51fbb9ff081cc75b
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-08 19:48:31 +0000

    Upadate 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *void more_numbers-Prints 10 times the numbers
+ *more_numbers-Prints 10 times the numbers
  *from 0 to 14, followed by a new line.
  */
 void more_numbers(void)
