d50d021c0b3b8ddedf1d544dd41863387508d847
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-08 19:44:57 +0000

    Upadate

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
- * print_most_numbers-print numbers from 0 to 9, followed by a new line.
- * Do not print 2 and 4
- */
+* print_most_numbers-print numbers from 0 to 9, followed by a new line.
+* Do not print 2 and 4
+*/
+
 void print_most_numbers(void)
 {
-	int c;
-	for (c = 0 ; c < 10 ; c++)
-		if (c != 2 && c != 4)
-			_putchar(c + '0');
-	_putchar('\n');
+int c;
+for (c = 0 ; c < 10 ; c++)
+if (c != 2 && c != 4)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
 }
