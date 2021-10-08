a9f53896d77103576534bcc3d3772a0be534499b
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-08 22:46:58 +0000

    Update Tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,12 @@
 
 void print_most_numbers(void)
 {
-int c;
-for (c = 0 ; c < 10 ; c++)
-if (c != 2 && c != 4)
-{
-_putchar(c + '0');
+	int c;
+	for (c = 0 ; c < 10 ; c++)
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+
