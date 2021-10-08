dacc37e539479d6c2097224c23037db2791a9074
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-08 22:33:57 +0000

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_line-Write a function that draws a straight line using the character _.
+*@n: the number of times the character _ should be printed
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+_putchar('_');
+_putchar('\n');
+}
