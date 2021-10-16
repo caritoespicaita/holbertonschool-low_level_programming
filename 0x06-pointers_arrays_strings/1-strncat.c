d64495fb9063b81d36be3955963086179fa92d97
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-15 22:44:12 -0500

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * _strncat - concatenates two strings
@@ -7,30 +6,18 @@
  * @n:length int
  * Return: dest
  */
-=======
-#include "holberton.h"
->>>>>>> 4627b561d72d5fcea409b1d755c0845cf3a9aeeb
-
-/**
- * _strncat - concatenates two strings
- * @dest:char dest
- * @src: char src
- * @n:length int
- * Return:char
- */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
-	return (dest);
+        for (i = 0; dest[i] != '\0'; i++)
+        {
+                continue;
+        }
+        for (j = 0; src[j] != '\0' && j < n; j++)
+        {
+                dest[i + j] = src[j];
+        }
+        dest[i + j] = '\0';
+        return (dest);
 }
-
