9516ab565ce3cd5aa776454a926134555e54fc38
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-17 23:01:29 -0500

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i;
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
