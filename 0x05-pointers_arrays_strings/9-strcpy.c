8d4a20df77025fd8272d65371f7a83fe4a976316
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-18 03:56:26 +0000

    Update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy -  a function that copies the string.
+ * @dest : character value
+ * @src  : character value
+ *
+ *Return: return the pointer to desk.
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest**;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
