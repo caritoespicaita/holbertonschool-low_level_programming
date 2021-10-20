8f3fd7b7debc0149d6e0f2602d866dfdada1a028
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-20 04:18:44 +0000

    Update 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * char *_strpbrk - searches a string for any of a set of bytes
+ * @s: first occurrence in the string
+ * @accept:check
+ * Return: pointer to byte in s that matches or NULL if no match.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if (accept[j] == s[i])
+				return (&s[i]);
+	}
+	return ('\0');
+}
+
