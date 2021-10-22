ef88ee0cf4c6eafb12f86a742dbbe455c341fccc
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-21 23:51:14 -0500

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,13 @@
 
 /**
  * _print_rev_recursion- prints a string in reverse..
- * @s: strin reverse
+ * @s: string reverse
  * Return: char.
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	_putchar(*s);
 	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
-
