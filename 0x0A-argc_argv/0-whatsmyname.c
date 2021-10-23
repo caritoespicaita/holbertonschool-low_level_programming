335c2eb2115f7ad9b1e852e05dd6f71c720f1a67
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-22 22:09:31 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
  * main - prints its name, followed by a new line.
  * @argv: argument count
@@ -12,6 +11,5 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s \n", argv[0]);
-	
 	return (0);
 }
