132d7c8c5d1065a3ec2c5cb84276b69eb6e84e00
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-23 03:23:07 +0000

    Update fix to fix 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * @argc: argument vector
  * Return: always 0.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s \n", argv[0]);
-	
+	(void)(argc);
+	printf("%s\n", argv[0]);
 	return (0);
 }
