bffd33af456760badd141ea191ee7738f5636c83
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-23 04:27:15 +0000

    Update fix 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
-
-	if (argc == 3)
-	{
-		mul = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", mul);
-	}
-	else
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+			return (0);
 }
      
