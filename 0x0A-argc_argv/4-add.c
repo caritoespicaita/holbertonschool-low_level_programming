1210b6479dbca5f954605aea3f1455b94bc70df4
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-25 04:43:37 +0000

    Update Tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers..
+ * @argv: argument count
+ * @argc: argument vector
+ * Return: 1 number contains symbols that are not digits.
+ */
+int main(int argc, char *argv[])
+{
+int i,  sum = 0;
+if (argc <= 2)
+printf("%d\n", 0);
+return (1);
+for (i = 1; i < argc; i++)
+{
+if (!atoi(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+sum = sum + atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
+
