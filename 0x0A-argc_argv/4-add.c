2eaf1921e02686000bfa8833a8b2fb5c033dd323
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-25 04:51:50 +0000

    Update fix add 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,20 +10,24 @@
  */
 int main(int argc, char *argv[])
 {
-int i,  sum = 0;
-if (argc <= 2)
-printf("%d\n", 0);
-return (1);
-for (i = 1; i < argc; i++)
-{
-if (!atoi(argv[i]))
-{
-printf("Error\n");
-return (1);
-}
-sum = sum + atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
+	int i,  sum = 0;
+
+	if (argc <= 2)
+	{
+		printf("%d\n", 0);
+	return (1);
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
