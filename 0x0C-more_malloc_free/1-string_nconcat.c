67076213d626a5b6985736bdab0772873ef0881d
caritoespicaita <caritoespicaita@hotmail.com>
2021-10-29 00:29:56 +0000

    Update fix 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		conc[i] = s1[i];
 	}
 
-	for (i = 0; i < l2; i++)
+	for (i = 0; i <= n && s2[i] = '\0'; i++)
 	{
 		conc[i + l1] = s2[i];
 	}
