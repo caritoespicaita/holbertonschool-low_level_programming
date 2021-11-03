fa4b429ad24a0a7eeb1a28f91ab4ac4778bc686f
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-03 04:55:59 +0000

    Update fix 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,26 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
+	dog_t *p;
+	/* reserving memory to struct*/
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 		return (NULL);
-	dog_t *new = malloc(sizeof(dog_t));
-	if (new == NULL)
+	/* Cpunting name pointer*/
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
 		return (NULL);
-	(*new).name = name;
-	(*new).age = age;
-	(*new).owner = owner;
-	return (new);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
