fd208e4d663ddaaa85f63f36c932d8d551b3fa15
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-04 22:29:54 -0500

    Upadate fix 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,21 +5,18 @@
  * @n: count
  * @separator: char
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list argum;
-	unsigned int i;
-
-	va_start(argum, n);
-
-	for (i = 0; i < n ; i++)
-	{
-	     if ( i < n - 1 && separator != NULL)
-             printf("%d%s", va_arg(argum, int), separator);
-             else
-	     printf("%d", va_arg(argum, int));
-	}
-	printf("\n");
-	va_end(argum);
+va_list argum;
+unsigned int i;
+va_start(argum, n);
+for (i = 0; i < n ; i++)
+{
+if (i < n - 1 && separator != NULL)
+printf("%d%s", va_arg(argum, int), separator);
+else
+printf("%d", va_arg(argum, int));
+}
+printf("\n");
+va_end(argum);
 }
