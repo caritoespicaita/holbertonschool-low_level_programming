70c630ee74bab7e900eda690e3b0d09d8ab237f8
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-16 18:33:41 -0500

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,12 +3,19 @@
 /**
  *get_nodeint_at_index - that deletes the head node of a linked list
  *@head: linked list
+ *@index: index of the node
  *Return: 0 if linked list is empty
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node = head;
 	unsigned int count = 0;
+
+	while (node)
+	{
+		count++;
+		node = node->next;
+	}
 
 	if (index >= count)
 	{
