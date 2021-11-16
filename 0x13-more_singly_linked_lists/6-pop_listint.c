32406889e6d7d17289a767df0f38f2785f339725
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-16 15:27:41 -0500

    Update Task 6.

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,16 +1,20 @@
 #include "lists.h"
 
-
-
-
+/**
+ *pop_listint-that deletes the head node of a linked list
+ *@head: linked list
+ *Return: 0 if linked list is empty
+ */
 int pop_listint(listint_t **head)
 {
-    listint_t *nodo = NULL;
-    int n;
+	listint_t *nodo = NULL;
+	int n;
 
-    nodo = (*head)->next;
-    n = (*head)->n;
-    free (*head);
-    *head = nodo;
-    return (n);
+	if (!h)
+		return (0);
+	nodo = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = nodo;
+	return (n);
 }
