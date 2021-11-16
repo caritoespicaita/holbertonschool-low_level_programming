b64e13d000af66f3175f237759a085506cd3864e
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-15 22:13:01 -0500

    Update task 5.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ *print_before_main - function that frees a  list.
+ *
+ */
+void print_before_main(void) __attribute__ ((constructor));
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n ");
+}
