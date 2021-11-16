216b9a921fce457dcaee720605421188cf449f33
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-15 21:08:27 -0500

    Update task 2.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *add_node - function that adds a new node at the 
+ * 			 beginning of a list_t list.
+ *@h: linked list
+ *@str: string
+ * Return: long linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	/*create a node*/
+	list_t *new = NULL;
+
+	new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+    
+    new->str = strdup(str);
+    new->len = strlen(str);
+    new->next = *head;
+    *head = new;
+    return (new);   
+}
+
+
+
+
+
