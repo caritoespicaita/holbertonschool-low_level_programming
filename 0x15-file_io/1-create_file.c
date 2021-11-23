a9a09d68b09d15a1962ddd76e6f9360b08bf1383
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-23 00:43:27 -0500

    fix code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (text_content != NULL)
 	{
-		for (size = 0; text_content[size]; size++)
+		for (size = 0; text_content[size] != '\0'; size++)
 			;
 	}
 	wr = write(file, text_content, size);
