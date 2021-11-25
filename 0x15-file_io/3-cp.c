222d341d8a831ddfe23d5c7b484f5e3f2e5f524e
caritoespicaita <caritoespicaita@hotmail.com>
2021-11-24 22:09:20 -0500

    Update 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+* main - copies the content of a file to another file
+* @:
+* @: 
+* Return: 
+*/
+int main(int ac, char **av)
+{
+    
+    int file_from, file_to, file_read;
+    char buf[1024];
+
+    if (ac != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    file_from = open(av[1],O_RDONLY);
+    if (file_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+        exit(98);
+    }
+    file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC);
+    if (file_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+        exit(99);
+    }
+    while ((file_read = read(file_from, buf, 1024)) > 0)
+    {
+        if (write(file_to, buf, file_read) != file_read)
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+    }
+    if (file_read == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+        exit(98);
+    }
+    if (close(file_from) == -1)
+     {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+        exit(98);
+    } 
+    if (close(file_to) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+        exit(98);
+    }
+    return(0);
+}
