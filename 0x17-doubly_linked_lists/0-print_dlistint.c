6416d9b8964e53e5287f661999b09d8324e893d9
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-06 21:24:45 -0500

    Fix betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 /**
- *print_list:-function print linked list
+ *print_dlistint:-function print double linked list
  *@h: linked list
  * Return: long linked list
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -16,6 +15,4 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 	}
 	return (count);
-
-
 }
