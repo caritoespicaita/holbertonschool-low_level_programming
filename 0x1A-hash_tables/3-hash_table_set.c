d844f23d896fd542faf4929d981d8259da4ff2ca
caritoespicaita <caritoespicaita@hotmail.com>
2022-01-30 21:17:46 -0500

    fix betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,44 +2,43 @@
 
 /**
 * hash_table_set - function that adds an element to the hash table.
-* @ht:  is the hash table you want to add 
+* @ht:is the hash table you want to add
 * @key: is the key
 * @value: is the value associated with the key
 * Return: the index
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index = 0;
-    hash_node_t *new_node = NULL;
+	unsigned long int index = 0;
+	hash_node_t *new_node = NULL;
 
-    if (!ht || !key || !value || *key == '\0')
-        return (0);
+	if (!ht || !key || !value || *key == '\0')
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = ht->array[index];
 
-    index = key_index((unsigned char*)key, ht->size);
-    new_node = ht->array[index];
+	while (new_node)
+	{
+		if (strcmp(key, new_node->key) == 0)
+		{
+			free(new_node->value);
+			new_node->value = strdup(value);
+			return (1);
+		}
+		new_node = new_node->next;
+	}
 
-    while (new_node)
-    {
-        if (strcmp(key, new_node->key) == 0)
-        {
-            free(new_node->value);
-            new_node->value = strdup(value);
-            return (1);
-        }
-        new_node = new_node->next;
-    }
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
-    
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 
-    if (ht->array[index] != NULL)
-        new_node->next = ht->array[index];
+	if (ht->array[index] != NULL)
+		new_node->next = ht->array[index];
 
-    ht->array[index] = new_node;
+	ht->array[index] = new_node;
 
-    return (1);
+	return (1);
 }
